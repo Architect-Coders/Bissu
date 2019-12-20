246c184079f709012f92a3604f5f8bf6eea5872f
calyr <calyr.software@gmail.com>
2019-12-20 15:44:27 -0400

    create profile activity

---
+++ app/src/main/java/com/architeccoders/bissu/ui/profile/ProfileActivity.kt
@@ -0,0 +1,23 @@
+package com.architeccoders.bissu.ui.profile
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.architeccoders.bissu.R
+
+import kotlinx.android.synthetic.main.activity_profile.*
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
