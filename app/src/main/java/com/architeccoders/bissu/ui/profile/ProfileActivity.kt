13a3ed660f2d803578d343d2d6ce6a62f3f88cdf
calyr <calyr.software@gmail.com>
2020-01-11 22:35:18 -0400

    rebase develop

--- app/src/main/java/com/architeccoders/bissu/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/architeccoders/bissu/ui/profile/ProfileActivity.kt
@@ -1,58 +1,36 @@
 package com.architeccoders.bissu.ui.profile
 
 import android.os.Bundle
-import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.architeccoders.bissu.R
-import com.architeccoders.bissu.ui.common.getViewModel
-import com.architeccoders.bissu.ui.login.LoginActivity
-import com.architeccoders.bissu.ui.login.fragments.CreateAccountFragment
-import com.architectcoders.domain.User
+import com.architeccoders.bissu.ui.profile.fragments.ProfileFragment
 
-import kotlinx.android.synthetic.main.activity_profile.*
-import kotlinx.android.synthetic.main.content_profile.*
+
 
 class ProfileActivity : AppCompatActivity() {
-    private lateinit var viewModel: ProfileViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        setSupportActionBar(toolbar)
-        val userTest = User(  username = "calyr",
-            password = "test",
-            firstName = "Roberto Carlos",
-            lastName = "Callisaya Mamani")
-        viewModel = getViewModel {
-            ProfileViewModel(userTest)
-        }
-        viewModel.model.observe(this, Observer { ::updateUi })
-
-        fab.setOnClickListener {
-            viewModel.onProfileEditClicked(userTest)
-        }
-
-        viewModel.doRefresh()
+        startProfileFragment()
     }
 
-    fun updateUi(model: ProfileViewModel.UiModel) {
-        when ( model ) {
-            is ProfileViewModel.UiModel.Navigation -> {
-                Toast.makeText(this.applicationContext, "Edit User Profile", Toast.LENGTH_LONG).show()
-            }
-            is ProfileViewModel.UiModel.Content -> {
-                with(model.user) {
-                    profile_username.text = username
-                    profile_name.text = firstName + lastName
-                }
-
-            }
-        }
+    private fun startProfileFragment() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = ProfileFragment()
+        fragmentTransaction.add(R.id.content_profile, fragment)
+        fragmentTransaction.commit()
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
 
 }
