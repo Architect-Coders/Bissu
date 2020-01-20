3ee9352beb11bdcd6e00a6d9d35b9b7b12d1efde
Anibal Cortez <anibalcortezarenas@gmail.com>
2020-01-19 22:54:46 -0300

    Solved issues in LoginDatasource.

--- app/src/main/java/com/architeccoders/bissu/data/server/login/LoginDatasource.kt
+++ app/src/main/java/com/architeccoders/bissu/data/server/login/LoginDatasource.kt
@@ -6,6 +6,8 @@ import com.architeccoders.bissu.data.server.RetrofitClient
 import com.architeccoders.bissu.data.server.login.request.LoginRequest
 import com.architectcoders.data.source.LoginRemoteDatasource
 import com.architectcoders.domain.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 /**
  * Created by Anibal Cortez on 2020-01-19.
@@ -13,14 +15,18 @@ import com.architectcoders.domain.User
 class LoginDatasource : LoginRemoteDatasource {
 
     override suspend fun createAccount(user: User, password: String): User? =
-        RetrofitClient().loginService
-            .register(user.toRequestUser(password))
-            .await()
-            .user?.toDomainUser()
+        withContext(Dispatchers.IO) {
+            RetrofitClient().loginService
+                .register(user.toRequestUser(password))
+                .await()
+                .user?.toDomainUser()
+        }
 
     override suspend fun doLogin(username: String, password: String): User? =
-             RetrofitClient().loginService
-            .doLogin(LoginRequest(username,password))
-            .await()
-            .user?.toDomainUser()
+        withContext(Dispatchers.IO){
+            RetrofitClient().loginService
+                .doLogin(LoginRequest(username,password))
+                .await()
+                .user?.toDomainUser()
+        }
 }
